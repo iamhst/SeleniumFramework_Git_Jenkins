40d96277c9820edd4fff2bdad1273f1bf959c7dc
Himanshu <iamhst@gmail.com>
2020-01-16 19:24:48 +0530

    Created a rough class to test setup is correct.

---
+++ DataDrivenFramework/src/test/java/rough/Rough.java
@@ -0,0 +1,15 @@
+package rough;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Rough {
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver", "//src//test//resources//Executables//chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+	}
+
+}
